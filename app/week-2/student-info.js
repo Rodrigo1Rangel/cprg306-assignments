d6f2420f4484694f4a6738e7a5750fde7af10333
Rodrigo Rangel <130096450+Rodrigo1Rangel@users.noreply.github.com>
2025-01-28 17:36:19 -0700

    Import StudentInfo into week-2 page.js

--- app/week-2/student-info.js
+++ app/week-2/student-info.js
@@ -1,6 +1,9 @@
+import Link from "next/link";
+
 export default function StudentInfo() {
+    return(
     <main>
         <p>Rodrigo Alves Rangel</p>
-        <Link href="https://github.com/Rodrigo1Rangel/cprg306-assignments.git" />
-    </main>
+        <Link href="https://github.com/Rodrigo1Rangel/cprg306-assignments.git">https://github.com</Link> </main>
+    )
 }
